908a8cb6795e10286839834a5317205aaaacea53
AleksanderGabov <gabov@it-limon.ru>
2020-07-17 10:29:47 +0500

    Revert "Java3_Lesson1"
    This reverts commit d989b4536e55cdfa7684b3726336d07785c0af8e.

--- Java3_Lesson1/src/TApple.java
+++
@@ -1,9 +0,0 @@
-public class TApple extends TFruit
-{
-    private static final float cWEIGHT_APPLE = 1.0f;
-
-    public TApple(String aDescription)
-    {
-        super(aDescription, cWEIGHT_APPLE);
-    }
-}
--- Java3_Lesson1/src/TOrange.java
+++
@@ -1,9 +0,0 @@
-public class TOrange extends TFruit
-{
-    private static final float cWEIGHT_ORANGE = 1.5f;
-
-    public TOrange(String aDescription)
-    {
-        super(aDescription, cWEIGHT_ORANGE);
-    }
-}
